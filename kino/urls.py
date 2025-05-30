e05fd86030052cd86f7bcbc0420cd81580050dd2
ismoil <ismoilumarzoda@gmail.com>
2025-05-30 10:36:34 +0500

    debug

--- kino/urls.py
+++ kino/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('film/<int:movie_id>/', views.movie_detail, name='movie_detail'),
     path('show/<int:show_id>/', views.show_seats, name='show_seats'),
     path('order/<int:show_id>/<int:seat_id>/', views.make_order, name='make_order'),
+    path('my-orders/', views.my_orders, name='my_orders'),
+    path('movie/<int:movie_id>/review/', views.add_review, name='add_review'),
+
 ]
--- kino/views.py
+++ kino/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from django.utils.timezone import now
+from django.contrib.auth.decorators import login_required
+from .forms import ReviewForm
+from django.contrib import messages
 
 def movie_list(request):
     movies = Movie.objects.filter(is_active=True)
@@ -40,18 +43,44 @@ def show_seats(request, show_id):
     }
     return render(request, 'cinema/show_seats.html', context)
 
+@login_required(login_url='login')
 def make_order(request, show_id, seat_id):
     if request.method == 'POST':
-        user_id = request.user.id if request.user.is_authenticated else 1
+        if not request.user.is_authenticated:
+            return redirect('login')  # Перенаправляем неавторизованных пользователей на страницу входа
+            
         seat = get_object_or_404(SeatPlace, id=seat_id)
         if seat.status == 'free':
             seat.status = 'booked'
             seat.save()
             Order.objects.create(
-                user_id=user_id,
+                user_id=request.user,  # Передаем объект пользователя, а не ID
                 show_id_id=show_id,
                 seatplace_id_id=seat_id,
                 payment_status='paid'  
             )
             return redirect('movie_list')
     return redirect('show_seats', show_id=show_id)
+
+@login_required
+def my_orders(request):
+    orders = Order.objects.filter(user_id=request.user).select_related('show_id', 'seatplace_id', 'show_id__movie_id')
+    return render(request, 'cinema/my_orders.html', {'orders': orders})
+
+@login_required
+def add_review(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.movie_id = movie
+            review.user_id = request.user
+            review.save()
+            messages.success(request, 'Ваш отзыв успешно добавлен!')
+            return redirect('movie_detail', movie_id=movie.id)
+    else:
+        form = ReviewForm()
+    
+    return render(request, 'cinema/add_review.html', {'form': form, 'movie': movie})
