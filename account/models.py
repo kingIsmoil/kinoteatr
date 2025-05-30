743d2e41bd0c8ca020106387e62031fa43fc1a7a
H4CKER <aqdodovirshod7@gmail.com>
2025-05-30 14:24:18 +0500

    updated files 5

--- account/models.py
+++ account/models.py
@@ -23,6 +23,7 @@ class MyUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    age = models.IntegerField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
